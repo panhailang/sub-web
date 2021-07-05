5b8eab8e4e318dce71cf23f3f107c504c2c781c2
panhailang <panhailang96@gmail.com>
2021-07-05 18:52:40 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "http://sub.gus.xin:25500/sub?" }],
         remoteConfig: [
           {
             label: "universal",
